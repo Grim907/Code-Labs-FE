915d23d9163652d009747669dc840e88d171c5da
Grim907 <pgrimshaw636@gmail.com>
2022-11-14 12:56:49 -0600

    Injecting the Logging Service into Components

--- Class-20/services-start/src/app/account/account.component.ts
+++ Class-20/services-start/src/app/account/account.component.ts
@@ -1,18 +1,21 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css']
+  Providers: [LoggingService]
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
   @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
+  loggingService: any;
 
 
   onSetTo(status: string) {
     this.statusChanged.emit({id: this.id, newStatus: status});
-    console.log('A server status changed, new status: ' + status);
+    this.loggingService.logStatusChange(Status);
   }
 }
--- Class-20/services-start/src/app/new-account/new-account.component.ts
+++ Class-20/services-start/src/app/new-account/new-account.component.ts
@@ -1,19 +1,22 @@
+import { ProviderAst } from '@angular/compiler';
 import { Component, EventEmitter, Output } from '@angular/core';
 import { LoggingService } from '../logging.service'
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css']
+  Providers: [LoggingService]
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  constructor(private loggingService: LoggingService) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
       status: accountStatus
     });
-    const service = new LoggingService();
-    service.logStatusChange(accountStatus)
+    this.loggingService.logStatusChange(accountStatus);
   }
 }
