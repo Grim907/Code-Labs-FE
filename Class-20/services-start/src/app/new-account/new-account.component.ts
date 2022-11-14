2ef28e8823ec1b320481362cba2abf3592849136
Grim907 <pgrimshaw636@gmail.com>
2022-11-14 12:50:10 -0600

    Creating a Logging Service + intro

---
+++ Class-20/services-start/src/app/new-account/new-account.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { LoggingService } from '../logging.service'
+@Component({
+  selector: 'app-new-account',
+  templateUrl: './new-account.component.html',
+  styleUrls: ['./new-account.component.css']
+})
+export class NewAccountComponent {
+  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  onCreateAccount(accountName: string, accountStatus: string) {
+    this.accountAdded.emit({
+      name: accountName,
+      status: accountStatus
+    });
+    const service = new LoggingService();
+    service.logStatusChange(accountStatus)
+  }
+}
