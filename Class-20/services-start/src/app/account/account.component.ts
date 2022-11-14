f5d1dd0affd6905d71a7fbcd17a11d243d7818f0
Grim907 <pgrimshaw636@gmail.com>
2022-11-14 13:15:44 -0600

    How many Instances of Service Should It Be?

--- Class-20/services-start/src/app/account/account.component.ts
+++ Class-20/services-start/src/app/account/account.component.ts
@@ -6,7 +6,7 @@ import { LoggingService } from '../logging.service';
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css']
-  Providers: [LoggingService, AccountService]
+  Providers: [LoggingService, ]
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string};
