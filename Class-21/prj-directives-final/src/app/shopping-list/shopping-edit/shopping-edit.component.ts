2b906f11ba3fe6cb0cde37c1a157438b6e2f3a1f
Grim907 <pgrimshaw636@gmail.com>
2022-11-15 13:23:22 -0600

    Adding the Shopping List Service

--- Class-21/prj-directives-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ Class-21/prj-directives-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -8,6 +8,7 @@ import {
 } from '@angular/core';
 
 import { Ingredient } from '../../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -17,9 +18,9 @@ import { Ingredient } from '../../shared/ingredient.model';
 export class ShoppingEditComponent implements OnInit {
   @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
   @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
-  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
-  constructor() { }
+
+  constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
   }
@@ -28,7 +29,7 @@ export class ShoppingEditComponent implements OnInit {
     const ingName = this.nameInputRef.nativeElement.value;
     const ingAmount = this.amountInputRef.nativeElement.value;
     const newIngredient = new Ingredient(ingName, ingAmount);
-    this.ingredientAdded.emit(newIngredient);
+    this.slService.addIngredient(newIngredient)
   }
 
 }
--- Class-21/prj-directives-final/src/app/shopping-list/shopping-list.component.ts
+++ Class-21/prj-directives-final/src/app/shopping-list/shopping-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
@@ -13,9 +14,10 @@ export class ShoppingListComponent implements OnInit {
     new Ingredient('Tomatoes', 10),
   ];
 
-  constructor() { }
+  constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
+    this.ingredients = this.slService.getIngredients();
   }
 
   onIngredientAdded(ingredient: Ingredient) {
