d8923caa606b28f88c92a7d7aa271ccf03ed6ca1
Grim907 <pgrimshaw636@gmail.com>
2022-11-15 13:37:18 -0600

    Adding Ingredients to Recipes

--- Class-21/prj-directives-final/src/app/recipes/recipe.service.ts
+++ Class-21/prj-directives-final/src/app/recipes/recipe.service.ts
@@ -1,9 +1,16 @@
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',[
+      new Ingredient ('meat', 1)
+      new Ingredient ('French Fries', 20)
+    ]),
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg', [
+      new Ingredient ('buns', 2)
+      new Ingredient ('Meat', 1)
+    ])
   ];
   recipeSelected: any;
 
