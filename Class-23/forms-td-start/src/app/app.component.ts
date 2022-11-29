6a9c125f0c370c35ca76620721dc4fc8f10d548d
Grim907 <pgrimshaw636@gmail.com>
2022-11-29 13:09:54 -0600

    Accessing the Form with @ViewChild

--- Class-23/forms-td-start/src/app/app.component.ts
+++ Class-23/forms-td-start/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,11 +7,16 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') signupForm: NgForm;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
-  onSubmit(form: NgForm) {
-    console.log('form')
-  }
+  //onSubmit(form: NgForm) {
+   // console.log('form')
+  //}
+  onSubmit(){
+    console.log(this.signupForm)
 
+  }
 }
