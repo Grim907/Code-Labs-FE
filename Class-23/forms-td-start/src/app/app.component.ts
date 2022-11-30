c686240fc19ac6240419bdd00c0e5fac27fa636a
Grim907 <pgrimshaw636@gmail.com>
2022-11-30 12:47:17 -0600

    Using ngModel with Two-Way-Binding

--- Class-23/forms-td-start/src/app/app.component.ts
+++ Class-23/forms-td-start/src/app/app.component.ts
@@ -8,7 +8,8 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
-  defualtQuestion = 'teacher'
+  defaultQuestion = 'teacher'
+  answer = ''
 
   suggestUserName() {
     const suggestedName = 'Superuser';
